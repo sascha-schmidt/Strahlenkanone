1f0e4f5123bed297fd1ddd0c95fdd8812c96a949
funkysash <saschaschmidt@arcor.de>
2010-01-21 17:24:26 +0100

    Stuff

---
+++ Strahlenkanone/build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,10 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp include/glutwindow.hpp \
+ include/rgb.hpp include/pixel.hpp include/ppmwriter.hpp
+
+include/glutwindow.hpp:
+
+include/rgb.hpp:
+
+include/pixel.hpp:
+
+include/ppmwriter.hpp:
---
+++ Strahlenkanone/build/Debug/GNU-Linux-x86/src/rgb.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux-x86/src/rgb.o: src/rgb.cpp include/rgb.hpp
+
+include/rgb.hpp:
