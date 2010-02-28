898bd2ff67f7c8e233e3cae117393117cc0eda01
funkysash <saschaschmidt@arcor.de>
2010-02-28 15:28:21 +0100

    UMMEL

---
+++ Strahlenkanone/include/light.hpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   light.hpp
+ * Author: sascha
+ *
+ * Created on 28. Februar 2010, 15:23
+ */
+
+#ifndef _LIGHT_HPP
+#define	_LIGHT_HPP
+
+#include <point3d.hpp>
+#include <rgb.hpp>
+
+struct light
+{
+public:
+
+};
+
+
+#endif	/* _LIGHT_HPP */
+
