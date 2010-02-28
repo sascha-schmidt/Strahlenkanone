2147ede4ad1b62764ac8a263993e2b7ff9550c38
glycoknob <mt@i3o.de>
2010-02-28 16:47:02 +0100

    egal

--- Strahlenkanone/build/Debug/GNU-Linux-x86/main.o.d
+++ Strahlenkanone/build/Debug/GNU-Linux-x86/main.o.d
@@ -1,5 +1,6 @@
 build/Debug/GNU-Linux-x86/main.o: main.cpp include/glutwindow.hpp \
- include/rgb.hpp include/pixel.hpp include/ppmwriter.hpp
+  include/rgb.hpp include/pixel.hpp include/ppmwriter.hpp \
+  ../../../include/GL/glut.h ../../../include/GL/freeglut_std.h
 
 include/glutwindow.hpp:
 
@@ -8,3 +9,7 @@ include/rgb.hpp:
 include/pixel.hpp:
 
 include/ppmwriter.hpp:
+
+../../../include/GL/glut.h:
+
+../../../include/GL/freeglut_std.h:
